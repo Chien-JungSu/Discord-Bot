623e0c4a35fc1eef64a2655ef36beb3b36dba3ca
Chien-JungSu <patricksu513166@gmail.com>
2026-09-13 01:41:01 +0800

    add html website

--- cogs/keep_alive.py
+++ cogs/keep_alive.py
@@ -1,20 +1,19 @@
+import os
 import threading
-from flask import Flask
-from threading import Thread
+from flask import Flask, render_template
 
-app = Flask('')
+app = Flask(__name__, template_folder='../templates')
 
 @app.route('/')
 def home():
-    return "機器人正在運作中！" # 這是網頁顯示的文字，你可以隨便改
+    return render_template('index.html')
+
 
 def run():
-    # 設定讓伺服器監聽 0.0.0.0，這樣 Render 才能偵測到
-    import os
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
 
 def keep_alive():
-    # 使用執行緒（Thread）讓網頁與機器人可以同時運作
-    t = threading.Thread(target=run)
+    t = threading.Thread(target=run, daemon=True)
     t.start()
